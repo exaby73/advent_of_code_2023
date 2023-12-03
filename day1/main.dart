e38e678712a0a22a7af95c92776703e4f13891af
exaby73 <nabeelparkar99@gmail.com>
2023-12-03 11:27:45 +0530

    Day 1 - Part 1

---
+++ day1/main.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  final input = File('input.part1.txt').readAsLinesSync();
+  int sum = 0;
+  for (final line in input) {
+    final chars = line.split('');
+    int? firstNumber;
+    int? lastNumber;
+    for (final char in chars) {
+      final number = int.tryParse(char);
+      if (number != null) {
+        if (firstNumber == null) {
+          firstNumber = number;
+        }
+        lastNumber = number;
+      }
+    }
+    sum += (firstNumber! * 10) + (lastNumber ?? firstNumber);
+  }
+  print(sum);
+}
